4cdbec61800265087fa9f9222f4076e445360d1a
katherine <static.katherine@gmail.com>
2012-09-06 11:45:39 -0400

    first commit of IVR & supporting files

--- empty.sh
+++ empty.sh
@@ -7,7 +7,7 @@
 	cd /var/spool/asterisk/tmp  #change to directory where file is temporarily saved
 	
 	# take newFile.wav and save it to custom directory with current time/date.wav
-	mv newFile.wav /usr/share/asterisk/sounds/myFile/$(date +%m%d-%H%M-%S).wav
+	mv newFile.wav /usr/share/asterisk/sounds/myFolder/$(date +%m%d-%H%M-%S).wav
 	
 
 #fi
